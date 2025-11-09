ec538f4adf7dc47ff806a40ec032120522ede481
Sayan Mondal <sm187966@gmail.com>
2025-11-09 07:26:26 +0000

    little more left side

--- src/components/second-card-decoration.tsx
+++ src/components/second-card-decoration.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function SecondCardDecoration() {
   return (
-    <div className="absolute -top-4 -left-16 z-10 w-48 h-48 pointer-events-none">
+    <div className="absolute -top-4 -left-20 z-10 w-48 h-48 pointer-events-none">
       <Image
         src="https://res.cloudinary.com/dlvoikod1/image/upload/v1762671985/VID-20251109-123336-unscreen_jpkqzf.gif"
         alt="Card Decoration"
