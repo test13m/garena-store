f36debbf608ca1afffadc2cca7bc4411159c32a1
Sayan Mondal <sm187966@gmail.com>
2025-11-09 07:24:55 +0000

    please make it little more big and move it little more left side but not

--- src/components/second-card-decoration.tsx
+++ src/components/second-card-decoration.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image';
 
 export default function SecondCardDecoration() {
   return (
-    <div className="absolute -top-6 -left-14 z-10 w-50 h-50 pointer-events-none">
+    <div className="absolute -top-4 -left-16 z-10 w-52 h-52 pointer-events-none">
       <Image
         src="https://res.cloudinary.com/dlvoikod1/image/upload/v1762671985/VID-20251109-123336-unscreen_jpkqzf.gif"
         alt="Card Decoration"
-        width={180}
-        height={180}
+        width={200}
+        height={200}
         className="object-contain"
       />
     </div>
